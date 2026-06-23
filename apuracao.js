a52bd70b9181fc5818aae55ab602fbef5d9c8703
Robinson Webber <robinsonwebber@hotmail.com>
2026-06-22 21:35:10 -0300

    Update Supabase client configuration and logic

--- apuracao.js
+++ apuracao.js
@@ -1,7 +1,10 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://iqfwbkfhpjngokwwthfr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_TcB_isEy54BfKu6LQHKTEw_g4QmO_nu";
+//const SUPABASE_URL = "https://iqfwbkfhpjngokwwthfr.supabase.co";
+//const SUPABASE_ANON_KEY = "sb_publishable_TcB_isEy54BfKu6LQHKTEw_g4QmO_nu";
+const SUPABASE_URL = "https://caweqdnkkcvbrmdglshe.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_P41Lf1XM-Lxn1OQ5Ew5orw_Teywojh6";
+
 
 const supabaseClient = createClient(
   SUPABASE_URL,
